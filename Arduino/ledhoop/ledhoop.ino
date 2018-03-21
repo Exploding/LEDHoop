7b0f0afa6ceef5fb4841500ebc05307c35dc2704
Alex <alexhors@uw.edu>
2018-03-21 10:10:58 -0700

    updated arduino code to read effects from SD card

--- Arduino/ledhoop/ledhoop.ino
+++ Arduino/ledhoop/ledhoop.ino
@@ -1,27 +1,52 @@
-#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
-#include <avr/power.h>
+//#include <avr/power.h>
 
-#define LED_PIN 0
-#define CTS_PIN 3
-#define RX_PIN 2
-#define N_LEDS 69
-#define FPS 30
+//#include <BlockDriver.h>
+//#include <FreeStack.h>
+//#include <MinimumSerial.h>
+#include <SPI.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <SysCall.h>
 
-SoftwareSerial serial(RX_PIN, 1);
+#define LED_PIN 5
+//#define CTS_PIN 3
+//#define RX_PIN 2
+#define N_LEDS 147
+#define FPS 50
+
+//SoftwareSerial serial(RX_PIN, 1);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, LED_PIN);
 
-uint8_t effectMode = 0,
-        effectPos  = 0;
+const uint8_t SOFT_MISO_PIN = 16;
+const uint8_t SOFT_MOSI_PIN = 14;
+const uint8_t SOFT_SCK_PIN  = 15;
+const uint8_t SD_CHIP_SELECT_PIN = 10;
+
+//SdFatSoftSpi<SOFT_MISO_PIN, SOFT_MOSI_PIN, SOFT_SCK_PIN> sd;
+SdFat sd;
+
+File file;
+
+uint8_t effectMode = 6,
+        effectPos  = 0,
+        frames = 10;
 uint8_t color[3] = {0,0,255};
 uint32_t prevTime = 0L;
+String fileName = "PAT0014.PAT";
+uint8_t buff[3];
 
 void setup() {
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-
-  serial.begin(9600);
+  Serial.begin(9600);
   
   strip.begin();
+  //strip.setBrightness(50);
+
+  if (!sd.begin(SD_CHIP_SELECT_PIN, SD_SCK_MHZ(50))) {
+    sd.initErrorHalt();
+  }
+
+  file.open(fileName.c_str());
 }
  
 void loop() {
@@ -34,24 +59,31 @@ void loop() {
       prevTime = t;
       break;  
     }
-    if(serial.available() > 0) {
-      cmd = (int) serial.read();
+    if(Serial.available() > 0) {
+      cmd = (int) Serial.read();  
 
       switch(cmd) {
-        case 0:
-          while(serial.available() < 3 && t + 10000 > micros());
-          if(serial.available() >= 3) {
-            color[0] = (uint8_t) serial.read();
-            color[1] = (uint8_t) serial.read();
-            color[2] = (uint8_t) serial.read();
+        case 34:
+          while(Serial.available() < 3 && t + 10000 > micros());
+          if(Serial.available() >= 3) {
+            color[0] = (uint8_t) Serial.read();
+            color[1] = (uint8_t) Serial.read();
+            color[2] = (uint8_t) Serial.read();
           }
           break;
-        case 1:
-          while(serial.available() < 1 && t + 10000 > millis());
-          if(serial.available() >= 1) {
-            effectMode = (uint8_t) serial.read();
+        case 35:
+          while(Serial.available() < 1 && t + 10000 > millis());
+          if(Serial.available() >= 1) {
+            effectMode = (uint8_t) Serial.read();
             effectPos = 0;
           }
+          break;
+        case 36:
+            file.close();
+            fileName = Serial.readStringUntil('!');
+            Serial.println("Received: " + fileName);
+            file.open(fileName.c_str());
+            frames = file.read();
           break;
       }
     }
@@ -80,6 +112,9 @@ void loop() {
       break;
     case 6:
       clearStrip();
+      break;
+    case 7:
+      pattern();
       break;
   }
 }
@@ -132,6 +167,23 @@ void solid() {
 void clearStrip() {
   for(int i = 0; i < N_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(0,0,0));
+  }
+}
+
+void pattern() {
+  if(file.isOpen()) {
+    file.seek((effectPos * N_LEDS * 3) + 1);
+    for(int i = 0; i < N_LEDS; i++) {
+      file.read(buff, sizeof(buff));
+      strip.setPixelColor(i, strip.Color(buff[0], buff[1], buff[2]));
+      //strip.setPixelColor(i, strip.Color((uint8_t)file.read(), (uint8_t)file.read(), (uint8_t)file.read()));
+    }
+
+    if(effectPos < frames - 1) {
+      effectPos++;
+    } else {
+      effectPos = 0;
+    }
   }
 }
 
